f62b11f31d865c38956b0e05382949e6c10974eb
Igor Muntyan <Igor.Muntyan@openlane.com>
2016-08-01 13:56:57 -0400

    websockets - functional

--- sink/src/main/java/com/openlane/bids/stream/BidConsumerSink.java
+++ sink/src/main/java/com/openlane/bids/stream/BidConsumerSink.java
@@ -9,22 +9,40 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.logging.Level;
 import lombok.extern.java.Log;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Component;
+
+import javax.inject.Inject;
 
 @Log
 @EnableBinding(BidConsumerMetadata.class)
+@Component
 public class BidConsumerSink {
 
     @Autowired
     ObjectMapper mapper;
 
+    @Inject
+    SimpMessageSendingOperations messagingTemplate;
 
     @StreamListener("bidstats")
     public void sink(BidStatsDto bidStats) {
         
         try {
             log.log(Level.INFO, this.mapper.writerWithDefaultPrettyPrinter().writeValueAsString(bidStats));
+            sendToClients(bidStats);
         } catch (Exception ex) {
             log.log(Level.SEVERE, "Exception thrown and could not map.", ex);
         }
     }
+
+    public void sendToClients(BidStatsDto dto) {
+        String json = "";
+        try {
+            json = mapper.writeValueAsString(dto);
+        } catch (Exception ex) {ex.printStackTrace();}
+        log.log(Level.INFO, "Sending to clients: " + json);
+        messagingTemplate.convertAndSend("/topic/tracking", json);
+    }
+
 }
