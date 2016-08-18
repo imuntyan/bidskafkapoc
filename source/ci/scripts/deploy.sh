713d102c311bd2a777cca80e32f7d1e14e7cbf76
Igor Muntyan <Igor.Muntyan@openlane.com>
2016-08-18 18:42:12 -0400

    debug deployment

--- source/ci/scripts/deploy.sh
+++ source/ci/scripts/deploy.sh
@@ -5,13 +5,13 @@ cd resource-bidskafkapoc/source/ci/scripts/kubernetes
 
 mkdir credentials
 
-echo $k8s_cluster_ca | base64 -d >> credentials/ca.pem
-echo $k8s_admin_cert | base64 -d >> credentials/admin.pem
-echo $k8s_admin_key | base64 -d >> credentials/admin-key.pem
+echo "$k8s_cluster_ca" | base64 -d >> credentials/ca.pem
+echo "$k8s_admin_cert" | base64 -d >> credentials/admin.pem
+echo "$k8s_admin_key" | base64 -d >> credentials/admin-key.pem
 
 ls -al
 
-echo $k8s_cluster_ca
+echo "$k8s_cluster_ca"
 
 cat credentials/ca.pem
 
