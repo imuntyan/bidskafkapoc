0304a6b67f529f79e78e3f2b52d53916938e104b
Igor Muntyan <Igor.Muntyan@openlane.com>
2016-08-18 18:11:35 -0400

    debug deployment

--- source/ci/scripts/deploy.sh
+++ source/ci/scripts/deploy.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
-cd bidskafkapoc-docker-image
+
+cd resource-bidskafkapoc/source/ci/scripts/kubernetes
+
+mkdir credentials
+
+base64 --decode --wrap=0 '$k8s_cluster_ca' >> credentials/ca.pem
+base64 --decode --wrap=0 '$k8s_admin_cert' >> credentials/admin.pem
+base64 --decode --wrap=0 '$k8s_admin_key' >> credentials/admin-key.pem
 
 ls -al
 
 
 dig k8s.kubernetes.net
+
+kubectl --kubeconfig=kubeconfig get nodes
