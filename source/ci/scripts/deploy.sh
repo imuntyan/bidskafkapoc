7dac28c5598ecf7e39b0ac28ca7643411d54e64f
Igor Muntyan <Igor.Muntyan@openlane.com>
2016-08-19 12:42:49 -0400

    debug deployment

--- source/ci/scripts/deploy.sh
+++ source/ci/scripts/deploy.sh
@@ -10,10 +10,4 @@ echo "$k8s_cluster_ca" | base64 -d >> credentials/ca.pem
 echo "$k8s_admin_cert" | base64 -d >> credentials/admin.pem
 echo "$k8s_admin_key" | base64 -d >> credentials/admin-key.pem
 
-ls -al
-
-echo "$k8s_cluster_ca"
-
-cat credentials/ca.pem
-
 kubectl --kubeconfig=kubeconfig run my-source --image=imuntyan/newwave:latest --replicas=2 --port=8090
--- source/ci/scripts/package.sh
+++ source/ci/scripts/package.sh
@@ -6,19 +6,7 @@ export TERM=${TERM:-dumb}
 
 gradle -Dorg.gradle.native=false build
 
-pwd
-
-cat /etc/hosts
-
-echo "$k8s_ip $k8s_dns_name" >> /etc/hosts
-
-cat /etc/hosts
-
 ls build/libs
-
-ls ../..
-
-ls ../../resource-jar
 
 cp build/libs/bidskafkapoc.jar ../../resource-jar
 
