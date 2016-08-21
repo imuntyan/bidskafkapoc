69cc0354859f1d3fa99a9529bbda386ee4d1890f
Igor Muntyan <Igor.Muntyan@openlane.com>
2016-08-21 15:33:50 -0400

    source as a kubernetes service

--- source/ci/scripts/deploy.sh
+++ source/ci/scripts/deploy.sh
@@ -10,4 +10,6 @@ echo "$k8s_cluster_ca" | base64 -d >> credentials/ca.pem
 echo "$k8s_admin_cert" | base64 -d >> credentials/admin.pem
 echo "$k8s_admin_key" | base64 -d >> credentials/admin-key.pem
 
-kubectl --kubeconfig=kubeconfig run my-source --image=imuntyan/newwave:latest --replicas=2 --port=8090
+kubectl --kubeconfig=kubeconfig apply -f service/service.yml
+
+kubectl --kubeconfig=kubeconfig apply -f service/cluster.yml
