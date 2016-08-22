a1b8a25497d4a1a6f19144b914086d6a59b5e200
Igor Muntyan <Igor.Muntyan@openlane.com>
2016-08-22 14:12:24 -0400

    kubernetes/concourse cleanup / debugging

--- source/ci/scripts/deploy.sh
+++ source/ci/scripts/deploy.sh
@@ -10,6 +10,8 @@ echo "$k8s_cluster_ca" | base64 -d >> credentials/ca.pem
 echo "$k8s_admin_cert" | base64 -d >> credentials/admin.pem
 echo "$k8s_admin_key" | base64 -d >> credentials/admin-key.pem
 
+kubectl config set DEPLOY_IMAGE_NAME "$deploy_image_name"
+
 kubectl --kubeconfig=kubeconfig apply -f service/service.yml
 
 kubectl --kubeconfig=kubeconfig apply -f service/cluster.yml
