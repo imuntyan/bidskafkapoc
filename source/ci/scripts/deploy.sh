09e80fc116392b8c45fe2f27a5acca177be59d1f
Igor Muntyan <Igor.Muntyan@openlane.com>
2016-08-22 16:07:33 -0400

    kubernetes/concourse parameterization

--- source/ci/scripts/deploy.sh
+++ source/ci/scripts/deploy.sh
@@ -10,7 +10,7 @@ echo "$k8s_cluster_ca" | base64 -d >> credentials/ca.pem
 echo "$k8s_admin_cert" | base64 -d >> credentials/admin.pem
 echo "$k8s_admin_key" | base64 -d >> credentials/admin-key.pem
 
-sed -e "s#\${APP_NAME}#$app_name#" -e "s#\${APP_NAME}#$app_port#" | kubectl --kubeconfig=kubeconfig apply -f -
+sed -e "s#\${APP_NAME}#$app_name#" -e "s#\${APP_PORT}#$app_port#" | kubectl --kubeconfig=kubeconfig apply -f -
 
 sed -e "s#\${DEPLOY_IMAGE_NAME}#$deploy_image_name#" \
 -e "s#\${APP_NAME}#$app_name#" service/cluster.yml \
