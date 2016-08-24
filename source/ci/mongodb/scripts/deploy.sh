76f059f58f66ad60978dfeffc1e522563e82959f
Igor Muntyan <Igor.Muntyan@openlane.com>
2016-08-24 11:15:42 -0400

    kubernetes/concourse parameterization

---
+++ source/ci/mongodb/scripts/deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+cd resource-bidskafkapoc/source/ci/mongodb
+
+mkdir credentials
+
+echo "$k8s_cluster_ca" | base64 -d >> credentials/ca.pem
+echo "$k8s_admin_cert" | base64 -d >> credentials/admin.pem
+echo "$k8s_admin_key" | base64 -d >> credentials/admin-key.pem
+
+
+sed -e "s#\${n}#1#" \
+  -e "s#\${volumeID}#$VOLUME_ID_1#" \
+  scripts/mongodb-service.yml | kubectl --kubeconfig=kubeconfig apply -f -
+
+sed -e "s#\${n}#2#" \
+  -e "s#\${volumeID}#$VOLUME_ID_2#" \
+  scripts/mongodb-service.yml | kubectl --kubeconfig=kubeconfig apply -f -
+
+sed -e "s#\${n}#3#" \
+  -e "s#\${volumeID}#$VOLUME_ID_3#" \
+  scripts/mongodb-service.yml | kubectl --kubeconfig=kubeconfig apply -f -
+
+sed -e "s#\${n}#1#" \
+  -e "s#\${volumeID}#$VOLUME_ID_1#" \
+  scripts/mongodb-cluster.yml | kubectl --kubeconfig=kubeconfig apply -f -
+
+sed -e "s#\${n}#2#" \
+  -e "s#\${volumeID}#$VOLUME_ID_2#" \
+  scripts/mongodb-cluster.yml | kubectl --kubeconfig=kubeconfig apply -f -
+
+sed -e "s#\${n}#3#" \
+  -e "s#\${volumeID}#$VOLUME_ID_3#" \
+  scripts/mongodb-cluster.yml | kubectl --kubeconfig=kubeconfig apply -f -
