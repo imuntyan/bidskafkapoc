5b4a584531e5a48ded01d0838d65eabf6f84baf6
Igor Muntyan <Igor.Muntyan@openlane.com>
2016-09-02 14:30:34 -0400

    redundancy goes first

--- source/ci/mongodb/scripts/deploy.sh
+++ source/ci/mongodb/scripts/deploy.sh
@@ -14,29 +14,29 @@ echo "$k8s_cluster_ca" | base64 -d >> credentials/ca.pem
 echo "$k8s_admin_cert" | base64 -d >> credentials/admin.pem
 echo "$k8s_admin_key" | base64 -d >> credentials/admin-key.pem
 
-
-sed -e "s#\${n}#1#" \
-  -e "s#\${volumeID}#$VOLUME_ID_1#" \
-  scripts/mongodb-service.yml | kubectl --kubeconfig=kubeconfig apply -f -
-
-sed -e "s#\${n}#2#" \
-  -e "s#\${volumeID}#$VOLUME_ID_2#" \
-  scripts/mongodb-service.yml | kubectl --kubeconfig=kubeconfig apply -f -
-
-sed -e "s#\${n}#3#" \
-  -e "s#\${volumeID}#$VOLUME_ID_3#" \
-  scripts/mongodb-service.yml | kubectl --kubeconfig=kubeconfig apply -f -
-
-sed -e "s#\${n}#1#" \
-  -e "s#\${volumeID}#$VOLUME_ID_1#" \
-  scripts/mongodb-cluster.yml | kubectl --kubeconfig=kubeconfig apply -f -
-
-sed -e "s#\${n}#2#" \
-  -e "s#\${volumeID}#$VOLUME_ID_2#" \
-  scripts/mongodb-cluster.yml | kubectl --kubeconfig=kubeconfig apply -f -
-
-sed -e "s#\${n}#3#" \
-  -e "s#\${volumeID}#$VOLUME_ID_3#" \
-  scripts/mongodb-cluster.yml | kubectl --kubeconfig=kubeconfig apply -f -
-
 kubectl --kubeconfig=kubeconfig apply -f scripts/mongodb-redundancy.yml
+
+sed -e "s#\${n}#1#" \
+  -e "s#\${volumeID}#$VOLUME_ID_1#" \
+  scripts/mongodb-service.yml | kubectl --kubeconfig=kubeconfig apply -f -
+
+sed -e "s#\${n}#2#" \
+  -e "s#\${volumeID}#$VOLUME_ID_2#" \
+  scripts/mongodb-service.yml | kubectl --kubeconfig=kubeconfig apply -f -
+
+sed -e "s#\${n}#3#" \
+  -e "s#\${volumeID}#$VOLUME_ID_3#" \
+  scripts/mongodb-service.yml | kubectl --kubeconfig=kubeconfig apply -f -
+
+sed -e "s#\${n}#1#" \
+  -e "s#\${volumeID}#$VOLUME_ID_1#" \
+  scripts/mongodb-cluster.yml | kubectl --kubeconfig=kubeconfig apply -f -
+
+sed -e "s#\${n}#2#" \
+  -e "s#\${volumeID}#$VOLUME_ID_2#" \
+  scripts/mongodb-cluster.yml | kubectl --kubeconfig=kubeconfig apply -f -
+
+sed -e "s#\${n}#3#" \
+  -e "s#\${volumeID}#$VOLUME_ID_3#" \
+  scripts/mongodb-cluster.yml | kubectl --kubeconfig=kubeconfig apply -f -
+
