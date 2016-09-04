6ac956276b251c2f5afda0b92f3a2b33f4295c23
Igor Muntyan <Igor.Muntyan@openlane.com>
2016-09-04 18:24:05 -0400

    kafka/zookeeper scripts

--- source/ci/kafka/scripts/deploy-kafka.sh
+++ source/ci/kafka/scripts/deploy-kafka.sh
@@ -8,7 +8,7 @@ echo "$k8s_cluster_ca" | base64 -d >> credentials/ca.pem
 echo "$k8s_admin_cert" | base64 -d >> credentials/admin.pem
 echo "$k8s_admin_key" | base64 -d >> credentials/admin-key.pem
 
-kubectl --kubeconfig=kubeconfig apply -f scripts/kafka-services.yml
+kubectl --kubeconfig=kubeconfig apply -f scripts/kafka-service.yml
 
 sleep 10
 
