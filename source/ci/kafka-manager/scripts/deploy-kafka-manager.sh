de6c0d157526754ada51023fc2702c12607aa8d3
Igor Muntyan <Igor.Muntyan@openlane.com>
2016-09-06 15:21:00 -0400

    kafka-manager

---
+++ source/ci/kafka-manager/scripts/deploy-kafka-manager.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cd resource-bidskafkapoc/source/ci/kafka
+
+mkdir credentials
+
+echo "$k8s_cluster_ca" | base64 -d >> credentials/ca.pem
+echo "$k8s_admin_cert" | base64 -d >> credentials/admin.pem
+echo "$k8s_admin_key" | base64 -d >> credentials/admin-key.pem
+
+kubectl --kubeconfig=kubeconfig apply -f scripts/kafka-manager-service.yml
+kubectl --kubeconfig=kubeconfig apply -f scripts/kafka-manager-cluster.yml
