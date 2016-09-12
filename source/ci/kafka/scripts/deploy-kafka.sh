543e58c3f95106edd88f048bb2c1bbfa80a8e940
Igor Muntyan <Igor.Muntyan@openlane.com>
2016-09-12 17:01:43 -0400

    kafka cluster - loop

--- source/ci/kafka/scripts/deploy-kafka.sh
+++ source/ci/kafka/scripts/deploy-kafka.sh
@@ -8,10 +8,14 @@ echo "$k8s_cluster_ca" | base64 -d >> credentials/ca.pem
 echo "$k8s_admin_cert" | base64 -d >> credentials/admin.pem
 echo "$k8s_admin_key" | base64 -d >> credentials/admin-key.pem
 
-sed -e "s#\${n}#1#" \
+for ORDINAL in 1 2
+do
+
+sed -e "s#\${n}#$ORDINAL#" \
   scripts/kafka-service.yml | kubectl --kubeconfig=kubeconfig apply -f -
-sed -e "s#\${n}#2#" \
-  scripts/kafka-service.yml | kubectl --kubeconfig=kubeconfig apply -f -
+
+done
+
 
 sleep 10
 
@@ -21,20 +25,27 @@ nohup kubectl --kubeconfig=kubeconfig proxy > proxy.log 2>&1 & echo $! > proxy.p
 # wait for proxy to start
 while ! grep "8001" -q proxy.log
 do sleep 0.1; done
-URL=http://127.0.0.1:8001/api/v1/namespaces/default/services/kafka-service
-public_ip=`curl -s $URL 2>&1 | grep '\"hostname\"' | awk '{print $2}' | awk -F\" '{print $2}'`
-kill `cat proxy.pid`
-echo $public_ip
 
+
+for ORDINAL in 1 2
+do
+
+URL="http://127.0.0.1:8001/api/v1/namespaces/default/services/kafka-service-$ORDINAL"
+public_ip=`curl -s $URL 2>&1 | grep '\"hostname\"' | awk '{print $2}' | awk -F\" '{print $2}'`
+echo $public_ip
 
 if [ ! -z "$public_ip" ]
 then
 sed -e "s#\${KAFKA_ADVERTISED_HOST_NAME}#$public_ip#" \
-  -e "s#\${n}#1#" \
-  scripts/kafka-deployment.yml | kubectl --kubeconfig=kubeconfig apply -f -
-sed -e "s#\${KAFKA_ADVERTISED_HOST_NAME}#$public_ip#" \
-  -e "s#\${n}#2#" \
+  -e "s#\${n}#$ORDINAL#" \
   scripts/kafka-deployment.yml | kubectl --kubeconfig=kubeconfig apply -f -
 else
-echo 'kafka cluster not created, /api/v1/namespaces/default/services/kafka-service did not return valid URL!'
+echo "kafka cluster not created, /api/v1/namespaces/default/services/kafka-service-$ORDINAL did not return valid URL!"
 fi
+
+done
+
+
+kill `cat proxy.pid`
+
+
